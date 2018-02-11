487847b52ebf795bc9ccd862b3499549e3ff7bf8
KarthikRao298 <karthikr298@gmail.com>
2018-02-11 11:41:39 -0500

    Scripts to load parameters
    - params_dynamic will load the various parameters required for the
     master-worker & advanced master-worker scheduler
    - params.sh will load the parametes required for the static & sequential scheduler

---
+++ params.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# File Name       :params.sh
+# Description     :Script to load the various parameters required for running on the cluster
+# Author          :Karthik Rao
+# Version         :0.1
+
+INTENSITIES="1 10 100 1000"
+NS="1000 1000000 1000000000"
+
+PROCS="1 2 4 8 16 32"
+
+RESULTDIR=result/
---
+++ params_dynamic.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# File Name       :params_dynamic.sh
+# Description     :Script to load the various parameters required for dynamic scheduler to run on the cluster
+# Author          :Karthik Rao
+# Version         :0.1
+
+INTENSITIES="1 10 100 1000"
+NS="1000 1000000 1000000000"
+
+PROCS="2 4 8 16 32"
+
+RESULTDIR=result/
+
